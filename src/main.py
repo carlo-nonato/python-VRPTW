37485378ffac6daded85d8c5c1a7cc1dbe6a5731
Carlo Nonato <carlo.nonato95@gmail.com>
2020-01-07 15:12:17 +0100

    Add argparse

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 import numpy as np
 from timeit import timeit
+import argparse as ap
 
 from parsers import solomon_parse
 from VRPTW import Customer, VRPTW
 
-def test():
-    vrptw = solomon_parse("R101.25.txt")
+def test(input_file):
+    vrptw = solomon_parse(input_file)
     vrptw.vehicles = 100
     vrptw.init_model()
     return vrptw.solve()
 
 if __name__ == "__main__":
-    print(timeit(test, number=1))
-    obj, paths = test()
+    parser = ap.ArgumentParser(description='Process some integers.')
+    parser.add_argument('input_file', type=str)
+    args = parser.parse_args()
+
+    print(timeit("test(args.input_file)", number=1, globals=globals()))
+
+    obj, paths = test(args.input_file)
     print(obj)
     for path in paths:
         print(path)
